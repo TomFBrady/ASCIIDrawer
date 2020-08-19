93bf79755ff3b95bb5ca1d8a3d957993047944e9
Tom Brady <tomfbrady1@gmail.com>
2020-08-20 00:19:16 +0100

    Replace magic number with resolution variable

--- ascii.py
+++ ascii.py
@@ -12,16 +12,28 @@ def myfunction(event):
         canvas.old_coords = None
 
 def my_button():
+    f = open("output.txt","w+")
+    f.write('.')
     for val in grid:
         isInBounds(val)
+        print (val)
+        if val[0] % 390 == 0 and val[0] != 0:
+            f.write('\n')
+        if(val in validSquares):
+            f.write('#.')
+            print (val)
+        else:
+            f.write('..')
+    f.close()
 
 def isInBounds(val):
     for coord in coordinates:
-        if coord[0] >= val[0] and coord[0] < val[0] + 10 and coord[1] >= val[1] and coord[1] < val[1] + 10:
+        if coord[0] >= val[0] and coord[0] < val[0] + res and coord[1] >= val[1] and coord[1] < val[1] + res:
             validSquares.add((val[0], val[1]))
-            canvas.create_rectangle(val[0], val[1], val[0] + 10, val[1]+10, outline="#000000", fill="#69f420")
+            canvas.create_rectangle(val[0], val[1], val[0] + res, val[1]+res, outline="#000000", fill="#69f420")
             break
 
+res = 5
 root = tk.Tk()
 canvas = tk.Canvas(root, width=400, height=400)
 canvas.pack()
@@ -32,11 +44,10 @@ validSquares = set()
 root.bind('<B1-Motion>', myfunction)
 root.bind('<ButtonRelease-1>', myfunction)
 
-for x in range(0, 400, 10):
-    for y in range(0, 400, 10):
-        grid.append((x,y))
+for x in range(0, 400, res):
+    for y in range(0, 400, res):
+        grid.append((y,x))
 
-print (coordinates)
 frame = tk.Frame(root)
 frame.pack()
 button = tk.Button(frame,
